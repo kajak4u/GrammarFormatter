09622157e6bc129d7e5a3031007e0e40b780225c
Krzysztof Krawczyk <krawczykk@hotmail.com>
2018-01-10 14:23:35 +0100

    Zgubiony plik z poprzedniego commitu.

--- Readable.h
+++ Readable.h
@@ -5,13 +5,10 @@
 //interface for stream reading/writing
 class IReadable
 {
-protected:
-	virtual _STD istream& ReadFrom(_STD istream& is) = 0;
-	virtual void WriteTo(_STD ostream& os) const = 0;
 public:
 	virtual ~IReadable();
-	friend _STD istream& operator >> (_STD istream& is, IReadable& obj);
-	friend _STD ostream& operator << (_STD ostream& os, const IReadable& obj);
+	virtual _STD istream& ReadFrom(_STD istream& is) = 0;
+	virtual void WriteTo(_STD ostream& os) const = 0;
 };
 _STD istream& operator>>(_STD istream& is, IReadable& obj);
 _STD ostream& operator<<(_STD ostream& os, const IReadable& obj);
